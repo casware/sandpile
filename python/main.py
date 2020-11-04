6c1d4c917478737380e38f46126f354424d9b3d8
Caleb Smith <caleb@atlasopen.com>
2020-11-04 21:10:38 +1100

    Added multiprocessing

--- python/main.py
+++ python/main.py
@@ -6,38 +6,52 @@ This is the main file from which we run all the various routines in the
 sandpile lab.
 
 """
-import time
 from pathlib import Path
 
 import matplotlib
 import numpy as np
-import scipy as sp
-import scipy.stats
-from matplotlib import pyplot
+from joblib import Parallel, delayed  # multi-processing
 
 from sandpile import SandPile
 from cylindrical import CylindricalSandPile
+from hourglass import HourGlassSandPile
 
-def example_plotting(pile, steps, output):
+
+def wrapper(i):
+    if i == 0:
+        pile = SandPile(20, 20, random=True)
+        return simulate_pile(pile, 100000, "results/s20x20/", no_mass=False)
+    if i == 1:
+        pile = CylindricalSandPile(20, 20, random=True)
+        return simulate_pile(pile, 100000, "results/c20x20/", no_mass=False)
+    if i == 2:
+        pile = HourGlassSandPile(20, 20, random=True)
+        return simulate_pile(pile, 100000, "results/hg20x20/", no_mass=False)
+    if i == 3:
+        pile = SandPile(50, 50, random=True)
+        return simulate_pile(pile, 100000, "results/s50x50/", no_mass=False)
+    if i == 4:
+        pile = CylindricalSandPile(50, 50, random=True)
+        return simulate_pile(pile, 100000, "results/c50x50/", no_mass=False)
+    if i == 5:
+        pile = HourGlassSandPile(50, 50, random=True)
+        return simulate_pile(pile, 100000, "results/hg50x50/", no_mass=False)
+    if i == 6:
+        pile = SandPile(50, 50, random=True)
+        return simulate_pile(pile, 100000, "results/s100x100/", no_mass=False)
+
+
+def simulate_pile(pile, steps, output, no_mass):
     pile.simulate(steps)
-    pile.graph(output)
+    return pile.graph(output, no_mass=no_mass)
 
-def ensemble_simulation(width, height, steps):
-    pile = SandPile(1,1)
-    pile.ensemble_simulate(width, height, steps)
 
 def main():
-    # Make sure that the output/ directory exists, or create it otherwise.
-    output_dir = Path.cwd() / "output"
+    output_dir = Path.cwd() / "results"
     if not output_dir.is_dir():
         output_dir.mkdir()
-
-    #pile = SandPile(100,100)
-    #example_plotting(pile, 100000, "standard100x100v2/")
-    pile = CylindricalSandPile(20, 20)
-    example_plotting(pile, 100000, "cylinder100x100/")
-    #ensemble_simulation(20,20, 10000)
-
+    results = Parallel(n_jobs=3)(delayed(wrapper)(i)
+                                   for i in range(6))
 
 
 if __name__ == "__main__":
